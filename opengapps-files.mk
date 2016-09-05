e33280936eae487e6eed38307877732cd2655fde
Sava Mikalacki <mikalackis@gmail.com>
2016-09-05 09:49:46 +0200

    Fix builds for bullhead (and potentially 6P)

--- opengapps-files.mk
+++ opengapps-files.mk
@@ -2,6 +2,8 @@
 PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,etc framework)
 
 # Pico and higher
-ifneq ($(filter $(TARGET_GAPPS_VARIANT),pico),)
-  PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,vendor/pittpatt usr/srec)
+ifeq ($(PRODUCT_DEVICE),hammerhead)
+   ifneq ($(filter $(TARGET_GAPPS_VARIANT),pico),)
+      PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,vendor/pittpatt usr/srec)
+   endif
 endif
